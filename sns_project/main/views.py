4877a786a48c63e6d58335ea805fa50feb2f8aac
SJ-stk <sangjune2000@naver.com>
2021-04-27 14:46:59 +0900

    update mainpage

--- sns_project/main/views.py
+++ sns_project/main/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def showmain(request):
     return render(request, "main/mainpage.html")
+
+
+def showhw(request):
+    return render(request, "main/show.html")
