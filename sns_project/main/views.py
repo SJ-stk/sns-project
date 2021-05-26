552f8f3ad0add0bb085fd632e6e98421e647a81e
tkdwns414 <sangjune2000@naver.com>
2021-05-26 22:17:31 +0900

    5th hw

--- sns_project/main/views.py
+++ sns_project/main/views.py
@@ -18,7 +18,7 @@ def favmu(request):
 
 def detail(request, id):
     post = get_object_or_404(Post, pk=id)
-    return render(request, "main/detail.html",{"post":post})
+    return render(request, "main/detail.html", {"post": post})
 
 
 def new(request):
@@ -31,6 +31,7 @@ def create(request):
     new_post.writer = request.POST["writer"]
     new_post.pub_date = timezone.now()
     new_post.body = request.POST["body"]
+    new_post.image = request.FILES["image"]
     new_post.save()
     return redirect("main:detail", new_post.id)
 
@@ -48,3 +49,9 @@ def update(request, id):
     update_post.body = request.POST["body"]
     update_post.save()
     return redirect("main:detail", update_post.id)
+
+
+def delete(request, id):
+    delete_post = Post.objects.get(id=id)
+    delete_post.delete()
+    return redirect("main:mainpage")
